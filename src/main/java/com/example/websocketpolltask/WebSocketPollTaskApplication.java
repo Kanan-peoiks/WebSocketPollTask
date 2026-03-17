e977b34c2ed00a76c4bc5cd6cdb88d65acc0f532
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-03-17 19:45:30 +0400

    Initial commit

---
+++ src/main/java/com/example/websocketpolltask/WebSocketPollTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketpolltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketPollTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketPollTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websocketpolltask/WebSocketPollTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketpolltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketPollTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
